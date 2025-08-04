ccbb236e6840d2a46e16fde1260457039d83ba08
Ivan Dagelic <dagelic.ivan@gmail.com>
2025-08-04 01:10:12 +0100

    chore: bump to 0.16.11
    Signed-off-by: Ivan Dagelic <dagelic.ivan@gmail.com>

--- daytona.rb
+++ daytona.rb
@@ -1,20 +1,20 @@
 class Daytona < Formula
     desc "Daytona CLI"
     homepage "https://daytona.io"
-    version "0.16.10"
+    version "0.16.11"
   
     if OS.mac? && Hardware::CPU.arm?
       url "https://download.daytona.io/cli/#{version}/daytona-darwin-arm64"
-      sha256 "1daa52a675e4809207058098730aa359ef3f10c9b96ea00f1c70d0d5473084fe"
+      sha256 "b87f4141bb675ea4abf91c135fcd47ebd88d36f7da45fdd51bab4b28c9af0f0b"
     elsif OS.mac? && Hardware::CPU.intel?    
       url "https://download.daytona.io/cli/#{version}/daytona-darwin-amd64"    
-      sha256 "d0a3ea60d4c138923371800a695c99e6f810174d7cc40937fd5f32c529d7df98"
+      sha256 "79baf28dba6fdc30dd8325f555164e8070a570685f76fd9b23d3df3255225ce0"
     elsif OS.linux? && Hardware::CPU.intel?
       url "https://download.daytona.io/cli/#{version}/daytona-linux-amd64"
-      sha256 "2bf9fcd5ad8efbed105ecf3c9478ba6754c9a703d585ef24df483536ace2b4c5"
+      sha256 "2a1fc60209f3ac6fb034fe6b5f8a3a31a73c7cde2503b74f2e5c6df87617f36b"
     elsif OS.linux? && Hardware::CPU.arm?
       url "https://download.daytona.io/cli/#{version}/daytona-linux-arm64"
-      sha256 "ffcf24d1f264a1cfe3033e1fc9c8c15156a97ed6c514b2b28ce7758eb7d2c3c2"
+      sha256 "b4a62a779b1e22c5594d44b2725f6b9dbd69138d19db68627ae846f9d288027c"
     else
       odie "Unsupported OS/ARCH combination"
     end
