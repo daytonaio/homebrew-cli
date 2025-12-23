1853e6a138eae582a713f29bb5ad838c98721620
Toma Puljak <toma.puljak@hotmail.com>
2025-12-23 11:36:03 +0100

    chore: add rc support
    Signed-off-by: Toma Puljak <toma.puljak@hotmail.com>

--- daytona-rc.rb
+++ daytona-rc.rb
@@ -1,20 +1,20 @@
 class Daytona < Formula
     desc "Daytona CLI"
     homepage "https://daytona.io"
-    version "v0.124.0"
+    version "v0.127.0"
   
     if OS.mac? && Hardware::CPU.arm?
       url "https://github.com/daytonaio/daytona/releases/download/#{version}/daytona-darwin-arm64"
-      sha256 "18f6d0a6e881ee32a3516045a7bf0dbe33a106b5497cf7411b044f7bbbb649b5"
+      sha256 "36060cbf59aeddeff5cf48d125122f2961bf2ee21b9efc76e5dd5e477aec3f98"
     elsif OS.mac? && Hardware::CPU.intel?
       url "https://github.com/daytonaio/daytona/releases/download/#{version}/daytona-darwin-amd64"
-      sha256 "b913b1508dd35a1b9a7d3a2bbbd5feeea8c78abb409c4261056fcd5ae39cc549"
+      sha256 "92361dceac021079ef934734e80fdb72b45ebee1e7ac1eb28434d19e0fa709a9"
     elsif OS.linux? && Hardware::CPU.intel?
       url "https://github.com/daytonaio/daytona/releases/download/#{version}/daytona-linux-amd64"
-      sha256 "0e4bfa7eace645ee8a1a9c67be9329e838832b7e1bf1f1654eac622c0cb51ccb"
+      sha256 "206ff5fa5e3442479c471f957a4228ba681510b348f0bd68f6fe80eb583e85f1"
     elsif OS.linux? && Hardware::CPU.arm?
       url "https://github.com/daytonaio/daytona/releases/download/#{version}/daytona-linux-arm64"
-      sha256 "6318eac609b058784d1bc111f8c193f825e8b5b34dee33925c4bdd276c23a9df"
+      sha256 "aaf09ac3ce04f03d2f79e54e8f545006bf901226fcae7ed60147476e9252a63f"
     else
       odie "Unsupported OS/ARCH combination"
     end
